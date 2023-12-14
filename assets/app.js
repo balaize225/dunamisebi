675652de6b2b7fd44f965fe97c9bf0bf0345c747
serge gbato <96743798+balaize225@users.noreply.github.com>
2023-12-14 16:17:45 +0100

    ajout tailwind

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,24 @@
-import './bootstrap.js';
+
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import './styles/app.scss'
 import './styles/app.css'
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+
+
+
+
+//you can specify wich plugins you need
+import { Tooltip, Toast, Popover } from 'bootstrap';
+
+//start the stimulus application
+import './bootstrap';
+
+
 
 // app.js
 
